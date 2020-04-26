0c0dc2b3bbd0558191dbf7f82726b2fce49e10ec
Alejandro Herrera <alejandro.herrera@pedidosya.com>
2020-04-26 17:29:10 -0300

    ip assign

--- tp-redes.cc
+++ tp-redes.cc
@@ -46,37 +46,50 @@ createChannels (NodeContainer emitters, NodeContainer routers, NodeContainer rec
   NodeContainer e0r0;
   e0r0.Add (emitters.Get (0));
   e0r0.Add (routers.Get (0));
-  pointToPoint.Install (e0r0);
+  NetDeviceContainer deve0r0 = pointToPoint.Install (e0r0);
 
   NodeContainer e1r0;
   e1r0.Add (emitters.Get (1));
   e1r0.Add (routers.Get (0));
-  pointToPoint.Install (e1r0);
+  NetDeviceContainer deve1r0 = pointToPoint.Install (e1r0);
 
   NodeContainer e2r0;
   e2r0.Add (emitters.Get (2));
   e2r0.Add (routers.Get (0));
-  pointToPoint.Install (e2r0);
+  NetDeviceContainer deve2r0 = pointToPoint.Install (e2r0);
 
-  pointToPoint.Install (routers);
+  NetDeviceContainer devRouters = pointToPoint.Install (routers);
 
   NodeContainer r1re0;
   r1re0.Add (routers.Get (1));
   r1re0.Add (receivers.Get (0));
-  pointToPoint.Install (r1re0);
+  NetDeviceContainer devr1re0 = pointToPoint.Install (r1re0);
 
   NodeContainer r1re1;
   r1re1.Add (routers.Get (1));
   r1re1.Add (receivers.Get (0));
-  pointToPoint.Install (r1re1);
+  NetDeviceContainer devr1re1 = pointToPoint.Install (r1re1);
 
   NodeContainer r1re2;
   r1re2.Add (routers.Get (2));
   r1re2.Add (receivers.Get (0));
-  pointToPoint.Install (r1re2);
+  NetDeviceContainer devr1re2 = pointToPoint.Install (r1re2);
 
   InternetStackHelper internet;
   internet.InstallAll ();
+
+  Ipv4AddressHelper ipv4;
+  ipv4.SetBase ("10.1.3.0", "255.255.255.0");
+
+  ipv4.Assign (deve0r0);
+  ipv4.Assign (deve1r0);
+  ipv4.Assign (deve2r0);
+  ipv4.Assign (devRouters);
+  ipv4.Assign (devr1re0);
+  ipv4.Assign (devr1re1);
+  ipv4.Assign (devr1re2);
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 }
 
 void
