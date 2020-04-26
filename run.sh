6502bf71b026f740922216cc101dce27afa22fe9
Alejandro Herrera <alejandro.herrera@pedidosya.com>
2020-04-26 16:47:53 -0300

    add simulation run

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-../../waf --run tp-redes
+../../waf --run tp-redes --cwd=examples/sor2-2020-tp2/output/
--- tp-redes.cc
+++ tp-redes.cc
@@ -1,8 +1,38 @@
+#include <fstream>
 #include "ns3/core-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/internet-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("TpRedes");
+
+void setupNodes ();
+void simulate ();
 
 int
 main (int argc, char const *argv[])
 {
-  std::cout << "hola";
+  setupNodes ();
+  simulate ();
   return 0;
+}
+
+void
+setupNodes ()
+{
+  std::cout << "Configurando nodos";
+  NS_LOG_INFO ("Create nodes.");
+  NodeContainer n;
+  n.Create (4);
+}
+
+void
+simulate ()
+{
+  NS_LOG_INFO ("Run Simulation.");
+  Simulator::Run ();
+  Simulator::Destroy ();
+  NS_LOG_INFO ("Done.");
 }
